2a1ce0db18552f89ac2d76457dd33da80f35bf2d
tsi1221 <tsehayneshbiruh2@gmail.com>
2026-03-14 07:45:46 -0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './', // your project root
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',          // your main React entry
+        chemicalreaction: './public/chapter1/chemicalreaction.html', // optional if you want to build it separately
+      }
+    }
+  }
 })
