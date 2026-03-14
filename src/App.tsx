856556658cc79899b153cd991fe11f8375f7f371
tsi1221 <tsehayneshbiruh2@gmail.com>
2026-03-14 08:50:21 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,107 @@
 export default function App() {
   return (
     <div>
-      <h1>Chemistry 3D Viewer</h1>
-      <a
-        href="/chapter1/chemicalreaction.html"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Open Chemical Reactions
-      </a>
+      <h1>Chemistry 3D chapter 1 Viewer</h1>
+  <span>1</span>  <a href="/chapter1/chemicalreaction.html" target="_blank" rel="noopener noreferrer">
+  Open Chemical Reactions
+</a>
+<br />
+  <span>2</span><a href="/chapter1/BOYLE’S LAW.html" target="_blank" rel="noopener noreferrer">
+  BOYLE’S LAW
+</a>
+<br />
+<span>3</span> <a href="/chapter1/atomicmodel.html" target="_blank" rel="noopener noreferrer">
+atomicmodel
+</a>
+ 
+ <br />
+
+ <span>4</span><a href="chapter1/Chemistry – The Central Science.html" target="_blank" rel="noopener noreferrer">
+Chemistry – The Central Science
+</a>
+<br />
+ <span>5</span><a href="chapter1/Photosynthesis.html" target="_blank" rel="noopener noreferrer">
+Photosynthesis
+</a>
+<br />
+<span>6</span><a href="chapter1/StateOfMatter.html" target="_blank" rel="noopener noreferrer">
+StateOfMatter
+</a>
+
+
+
+  <h1>Chemistry 3D chapter 2 Viewer</h1>
+<span>1</span><a href="chapter2/Density Concept (Mass ÷ Volume).html" target="_blank" rel="noopener noreferrer">
+Density Concept (Mass ÷ Volume)
+</a>
+<br />
+
+<span>2</span><a href="chapter2/PrecisionvsAccuracyDartboard.html" target="_blank" rel="noopener noreferrer">
+PrecisionvsAccuracyDartboard
+</a>
+
+
+
+
+<br />
+
+<span>3</span><a href="chapter2/Rounding and Calculations Lab.html" target="_blank" rel="noopener noreferrer">
+Rounding and Calculations Lab
+</a>
+
+
+<br />
+
+<span>4</span><a href="chapter2/SI Base Units.html" target="_blank" rel="noopener noreferrer">
+SI Base Units
+</a>
+
+
+<br />
+
+<span>5</span><a href="chapter2/SI Prefixes Scale.html" target="_blank" rel="noopener noreferrer">
+SI Prefixes Scale
+</a>
+
+<br />
+
+<span>6</span><a href="chapter2/Significant Figures Ruler .html" target="_blank" rel="noopener noreferrer">
+Significant Figures Ruler 
+</a>
+
+<br />
+
+<span>7</span><a href="chapter2/ DerivedUnitsArea&Volum Builder .html" target="_blank" rel="noopener noreferrer">
+  Derived Units "Area & Volume" Builder
+</a>
+
+
+<br />
+
+<span>8</span><a href="chapter2/
+ScientificNotationConverter .html" target="_blank" rel="noopener noreferrer"> 
+ScientificNotationConverter
+</a>
+
+<br />
+
+<span>9</span><a href="chapter2/SpeedandAccelerationGrapher.html" target="_blank" rel="noopener noreferrer">
+ SpeedandAccelerationGrapher
+</a>
+
+
+      <h1>Chemistry 3D chapter 3 Viewer</h1>
+
+
+
+<span>1</span><a href="chapter3/Isotopes and Mass Spectrometry.html" target="_blank" rel="noopener noreferrer">
+ Isotopes and Mass Spectrometry
+</a>
+<br />
+<span>2</span><a href="chapter3/1. Thomson’s Plum Pudding vs. Rutherford’s Gold Foil.html" target="_blank" rel="noopener noreferrer">
+Thomson’s Plum Pudding vs. Rutherford’s Gold Foil
+</a>
+
     </div>
   );
 }
